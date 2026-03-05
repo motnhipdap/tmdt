58467f827d19e5988cb03bfc4e8e9aee12d1a6a1
dungcony <dung090804@gmail.com>
2026-03-05 12:45:36 +0700

    update code

---
+++ src/main/java/com/dev/dungcony/commons/bases/ControllerBase.java
@@ -0,0 +1,12 @@
+package com.dev.dungcony.commons.bases;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+
+@RestController
+@Slf4j
+public abstract class ControllerBase {
+}
---
+++ src/main/java/com/dev/dungcony/commons/bases/ServiceBase.java
@@ -0,0 +1,12 @@
+package com.dev.dungcony.commons.bases;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+public abstract class ServiceBase {
+
+}
